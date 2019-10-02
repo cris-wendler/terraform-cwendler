d1292cfcb1c1ec04e3e1c487b7123d4881e81b50
Cristiano Wendler <cristianowendler@Cristianos-MacBook-Pro.local>
2019-10-01 21:52:59 -0300

    edited

--- resources.tf
+++ resources.tf
@@ -1,13 +1,13 @@
-resource "google_compute_network" "our_development_network" {
-  name = "devnetwork"
+resource "google_compute_network" "our_gcp_dev_vpc_one" {
+  name = "terraform-gcp-vpc-one"
   auto_create_subnetworks = true
 }
 
-resource "aws_vpc" "environment-example-two" {
+resource "aws_vpc" "our_aws_dev_vpc_one" {
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = {
-        Name = "terraform-aws-vpc-example-one"
+        Name = "terraform-aws-vpc-one"
     }
 }
